26f0b4061b937f46324c8ebd543fbe0b58194b93
Steph Khaguli <24970338+skha10@users.noreply.github.com>
2025-11-23 02:57:05 +0300

    CDN deployment: React and Tailwind via CDN, no build process

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// React is now available globally via CDN
+const { React, ReactDOM } = window;
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  React.createElement(React.StrictMode, null,
+    React.createElement(App)
+  )
+);
